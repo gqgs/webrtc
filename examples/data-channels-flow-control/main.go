cc5621ff3bacc3034b85a394d388e07aff9fc931
Hugo Arregui <hugo.arregui@gmail.com>
2019-09-24 20:39:24 -0300

    sharing example

--- examples/data-channels-flow-control/main.go
+++ examples/data-channels-flow-control/main.go
@@ -5,13 +5,11 @@ import (
 	"log"
 	"sync/atomic"
 	"time"
+	"os"
+	"io"
 
 	"github.com/pion/webrtc/v2"
-)
-
-const (
-	bufferedAmountLowThreshold uint64 = 512 * 1024  // 512 KB
-	maxBufferedAmount          uint64 = 1024 * 1024 // 1 MB
+	"github.com/pion/datachannel"
 )
 
 func check(err error) {
@@ -30,17 +28,63 @@ func setRemoteDescription(pc *webrtc.PeerConnection, sdp []byte) {
 	check(err)
 }
 
+type FlowControlledDC struct {
+	bufferedAmountLowThreshold uint64
+	maxBufferedAmount          uint64
+	bufferedAmountLowSignal    chan struct{}
+	dc                         *webrtc.DataChannel
+	detachedDC                 datachannel.ReadWriteCloser
+	totalBytesReceived uint64
+}
+
+// NewFlowControlledDC --
+func NewFlowControlledDC(dc *webrtc.DataChannel, bufferedAmountLowThreshold uint64, maxBufferedAmount uint64) (*FlowControlledDC, error) {
+	dcrwc, err := dc.Detach()
+	if err != nil {
+		return nil, err
+	}
+	fcdc := &FlowControlledDC{
+		dc:                         dc,
+		detachedDC:                 dcrwc,
+		bufferedAmountLowThreshold: bufferedAmountLowThreshold,
+		maxBufferedAmount:          maxBufferedAmount,
+		bufferedAmountLowSignal:    make(chan struct{}),
+	}
+	dc.SetBufferedAmountLowThreshold(bufferedAmountLowThreshold)
+	dc.OnBufferedAmountLow(func() {
+		fcdc.bufferedAmountLowSignal <- struct{}{}
+	})
+	return fcdc, nil
+}
+
+func (fcdc *FlowControlledDC) Read(p []byte) (int, error) {
+	n := len(p)
+	atomic.AddUint64(&fcdc.totalBytesReceived, uint64(n))
+	return fcdc.detachedDC.Read(p)
+}
+
+func (fcdc *FlowControlledDC) Write(p []byte) (int, error) {
+	if fcdc.dc.BufferedAmount() > fcdc.maxBufferedAmount {
+		<-fcdc.bufferedAmountLowSignal
+	}
+	return fcdc.detachedDC.Write(p)
+}
+
 func createOfferer() *webrtc.PeerConnection {
+	s := webrtc.SettingEngine{}
+	s.DetachDataChannels()
+
+	// Create an API object with the engine
+	api := webrtc.NewAPI(webrtc.WithSettingEngine(s))
+
 	// Prepare the configuration
 	config := webrtc.Configuration{
 		ICEServers: []webrtc.ICEServer{},
 	}
 
 	// Create a new PeerConnection
-	pc, err := webrtc.NewPeerConnection(config)
+	pc, err := api.NewPeerConnection(config)
 	check(err)
-
-	buf := make([]byte, 1024)
 
 	ordered := false
 	maxRetransmits := uint16(0)
@@ -50,36 +94,27 @@ func createOfferer() *webrtc.PeerConnection {
 		MaxRetransmits: &maxRetransmits,
 	}
 
-	sendMoreCh := make(chan struct{})
-
 	// Create a datachannel with label 'data'
 	dc, err := pc.CreateDataChannel("data", options)
 	check(err)
 
 	// Register channel opening handling
 	dc.OnOpen(func() {
-		log.Printf("OnOpen: %s-%d. Start sending a series of 1024-byte packets as fast as it can\n", dc.Label(), dc.ID())
+		// log.Printf("OnOpen: %s-%d. Start sending a series of 1024-byte packets as fast as it can\n", dc.Label(), dc.ID())
+		flowControlledDC, err := NewFlowControlledDC(dc, 512*1024, 1024*1024)
+		check(err)
 
-		for {
-			err2 := dc.Send(buf)
-			check(err2)
-
-			if dc.BufferedAmount()+uint64(len(buf)) > maxBufferedAmount {
-				// Wait until the bufferedAmount becomes lower than the threshold
-				<-sendMoreCh
-			}
+		f, err := os.Open("./test.mp4")
+		if err != nil {
+			panic(err)
 		}
+		info, err := f.Stat()
+		if err != nil {
+			panic(err)
+		}
+		log.Println(info.Size())
+		io.Copy(flowControlledDC, f)
 	})
-
-	// Set bufferedAmountLowThreshold so that we can get notified when
-	// we can send more
-	dc.SetBufferedAmountLowThreshold(bufferedAmountLowThreshold)
-
-	// This callback is made when the current bufferedAmount becomes lower than the threadshold
-	dc.OnBufferedAmountLow(func() {
-		sendMoreCh <- struct{}{}
-	})
-
 	return pc
 }
 
@@ -89,30 +124,46 @@ func createAnswerer() *webrtc.PeerConnection {
 		ICEServers: []webrtc.ICEServer{},
 	}
 
+	s := webrtc.SettingEngine{}
+	s.DetachDataChannels()
+
+	// Create an API object with the engine
+	api := webrtc.NewAPI(webrtc.WithSettingEngine(s))
 	// Create a new PeerConnection
-	pc, err := webrtc.NewPeerConnection(config)
+	pc, err := api.NewPeerConnection(config)
 	check(err)
 
 	pc.OnDataChannel(func(dc *webrtc.DataChannel) {
-		var totalBytesReceived uint64
+		// var totalBytesReceived uint64
 
 		// Register channel opening handling
 		dc.OnOpen(func() {
-			log.Printf("OnOpen: %s-%d. Start receiving data", dc.Label(), dc.ID())
-			since := time.Now()
+			flowControlledDC, err := NewFlowControlledDC(dc, 512*1024, 1024*1024)
+			check(err)
 
-			// Start printing out the observed throughput
-			for range time.NewTicker(1000 * time.Millisecond).C {
-				bps := float64(atomic.LoadUint64(&totalBytesReceived)*8) / time.Since(since).Seconds()
-				log.Printf("Throughput: %.03f Mbps", bps/1024/1024)
+			go func() {
+				log.Printf("OnOpen: %s-%d. Start receiving data", dc.Label(), dc.ID())
+				since := time.Now()
+
+				// Start printing out the observed throughput
+				for range time.NewTicker(1000 * time.Millisecond).C {
+					bps := float64(atomic.LoadUint64(&flowControlledDC.totalBytesReceived)*8) / time.Since(since).Seconds()
+					log.Printf("Throughput: %.03f Mbps, totalBytesReceived: %d", bps/1024/1024, flowControlledDC.totalBytesReceived)
+				}
+			}()
+			f, err := os.OpenFile("/tmp/foo.txt", os.O_CREATE|os.O_RDWR, 0666)
+			if err != nil {
+				log.Fatal(err)
 			}
+			io.Copy(f, flowControlledDC)
+
 		})
 
-		// Register the OnMessage to handle incoming messages
-		dc.OnMessage(func(dcMsg webrtc.DataChannelMessage) {
-			n := len(dcMsg.Data)
-			atomic.AddUint64(&totalBytesReceived, uint64(n))
-		})
+		// // Register the OnMessage to handle incoming messages
+		// dc.OnMessage(func(dcMsg webrtc.DataChannelMessage) {
+		// 	n := len(dcMsg.Data)
+		// 	atomic.AddUint64(&totalBytesReceived, uint64(n))
+		// })
 	})
 
 	return pc
